9c224893c5da5d79f9c73dd768c4f6213e1de640
flamendO <pik5874587@outlook.fr>
2024-04-10 08:54:17 +0200

    gui modif

--- src/gui.py
+++ src/gui.py
@@ -11,16 +11,24 @@ import matplotlib.pyplot as plt
 import os
 from wing_extraction import wing_extraction
 from rotation_aile import rotate_wing
+from tkinter.font import Font
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("green")
+# customtkinter.set_appearance_mode("dark")
+# customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
+root.attributes("-fullscreen", False)  # Mettre en plein écran
 root.geometry("1000x600")
+root.resizable(False, False)
 
+bg="black"
+root.config(bg=bg)
 
 filename = ""
 progress_bar = None
+
+def exit():
+    root.destroy()
 
 def importer():
     global filename
@@ -28,30 +36,37 @@ def importer():
 
 def open_file_dialog():
     global filename
+    global label1  # Déclarer label1 comme global pour pouvoir le réutiliser dans cette fonction
     filename = filedialog.askopenfilename()
     if filename:
-        image=customtkinter.CTkImage(light_image=Image.open(filename), dark_image=Image.open(filename), size=(400,250))
+        # Supprimer l'image précédente si elle existe
+        if 'label1' in globals():
+            label1.destroy()
+        image = customtkinter.CTkImage(light_image=Image.open(filename), dark_image=Image.open(filename), size=(400, 250))
         label1 = customtkinter.CTkLabel(root, text="", image=image)
-        label1.place(x=100,y=150)
+        label1.pack(expand=True, pady=20, padx=60)
         return filename
-    
-def analyse():
-    global progress_bar
-    if filename == "":
-        tk.messagebox.showerror("Erreur", message="Aucun fichier sélectionné")
-    else:
-        progress_bar = ttk.Progressbar(root, orient="horizontal", length=400, mode="determinate")
-        progress_bar.place(x=300, y=450)
-        progress_bar["value"] = 0  # Initialiser la valeur de la barre à 0
 
-        # Exécuter le processus d'analyse dans un thread séparé
-        analysis_thread = threading.Thread(target=perform_analysis)
-        analysis_thread.start()
+
+def open_bar():
+    global progress_bar
+    progress_window = tk.Toplevel(root)
+    progress_window.title("Generating")
+    progress_window.geometry("300x100")
+    
+    progress_bar = ttk.Progressbar(progress_window, orient="horizontal", length=200, mode="indeterminate")
+    progress_bar.pack(pady=20)
+    perform_analysis()
+    progress_window.destroy()
+
+
 
 def perform_analysis():
     global progress_bar
     global images_list
     images_list=[]
+
+    progress_bar.start()
     
     # Chemin du fichier 
     path = filename
@@ -68,55 +83,58 @@ def perform_analysis():
         images_list.append(np.array(image_tmp))
         #print(os.path.join(os.getcwd(), str(i) + '.png'))
         
-        
         progress_value = (i / indice) * 100 # mise a jour barre de progression
-        progress_bar["value"] = progress_value
+        # progress_bar["value"] = progress_value
         root.update_idletasks()
     os.chdir("../")
+    progress_bar.stop()
+    
 
     # plt.imshow(images_list[12])
     # plt.show()
 
 
-
-frame = customtkinter.CTkFrame(master=root)
-frame.grid(row=0, column=0, stick='news')
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-label = customtkinter.CTkLabel(master=frame, text="Projet Abeille", font=("Roboto",56))
-label.pack(pady=12, padx=10)
+def results():
+    print("A FAIRE")
 
 
 
+# frame = customtkinter.CTkFrame(master=root)
+# frame.pack(pady=10, padx=10, expand=False)
 
 
+####### LOGO
 
+img_logo = Image.open("./images/logo.png")
+resized_logo = img_logo.resize((100,100), Image.Resampling.LANCZOS)
+img_logo_resized = ImageTk.PhotoImage(resized_logo)
 
+logo = tk.Label(root, image=img_logo_resized, bg=bg, fg="white" )
 
-button1 = customtkinter.CTkButton(master=frame, text="Importer", command=open_file_dialog)
+logo.pack(side=tk.TOP,pady=10)
 
-button1.pack(pady=300, padx=0)
-button1.place(x=100,y=500)
+# label = customtkinter.CTkLabel(master=frame, text="Projet Abeille", font=("Roboto", 56))
+# label.pack(pady=12, padx=10)
+########
 
-button2 = customtkinter.CTkButton(master=frame, text="Analyse", command=analyse)
-button2.place(x=350,y=500)
-#button2.pack(expand=True)
+font1=customtkinter.CTkFont(family="Sans serif", size=20, weight="bold")
 
-# button1.grid(row=2, column=0)
+# button_frame = customtkinter.CTkFrame(master=root)  # Créer un cadre pour les boutons
+# button_frame.pack(side=tk.BOTTOM, pady=80, padx=100)  # Ajouter une marge en haut
 
+button1 = customtkinter.CTkButton(master=root, text="IMPORTER", command=open_file_dialog, font=font1)
+button1.place(x=50, y=150)
 
-## FRAME 1
+button2 = customtkinter.CTkButton(master=root, text="ANALYSE", command=open_bar, font=font1)
+button2.place(x=300, y=150)
 
-frame1 = customtkinter.CTkFrame(master=root, fg_color="#8D6F3A", border_color="#FFCC70", border_width=2)
+button3 = customtkinter.CTkButton(master=root, text="RÉSULTATS", command=results, font=font1)
+button3.place(x=550, y=150)
 
-frame.pack(expand=True)
+button4 = customtkinter.CTkButton(master=root, text="QUITTER", command=exit, font=font1)
+button4.place(x=800, y=150)
+
 
 
 root.mainloop()
-
-
-
-
 
